f6e417dc62534f48a7fe48bcc7217ae1914c1611
Monami Sato <monami.sato@yahoo.com>
2016-06-27 20:37:27 +0900

    the go back to menu button works now!!!!!!

---
+++ Assets/Scripts/gotomenubutton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class gotomenubutton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+	public void StartButton() {
+		SceneManager.LoadScene("menu");
+		GameObject.Find ("button").GetComponent<CanvasRenderer> ().SetAlpha (0);
+	}
+}
